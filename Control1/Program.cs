f15e80bfd1cc34ba94ef6ad9748fb2c5eb9ed3c0
RuslanKhasyanov <99578452+RuslanKhasyanov@users.noreply.github.com>
2022-05-22 18:47:05 +0300

    Update Program.cs

--- Control1/Program.cs
+++ Control1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Control
+{
+
+    public static string[] newArray()
+    {
+        string[] strs = { "tolli", "liberhaus", "lol", "12", "1", "nos", "tratata",
+        "fafafafa", "gohome", "ololo", "kompot", "989"};
+
+        string[] newStrs;
+        int count = 0;  
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i].Length <= 3) count++;
+        }
+
+        newStrs = new string[count];
+        count = 0;
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i].Length <= 3)
+            {
+                newStrs[count++] = strs[i];
+            }
+        }
+
+        return newStrs;
+    }
+}
